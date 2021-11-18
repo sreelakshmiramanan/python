099d578314b30209f530e27b3926d8d5edded451
sreelakshmipr162@gmail.com <sreelakshmipr162@gmail.com>
2021-11-18 11:05:13 +0530

    commit

---
+++ stfu.py
@@ -0,0 +1,59 @@
+#demo of len(string)
+s='Learning python is fun!'
+print("Length of", s, "is", len(s))
+
+#demoof lower()
+s= 'Learning python is fun!'
+print(s.lower())
+
+#demo of upper()
+s= 'Learning python is fun!'
+print(s.upper())
+
+
+#demo of swapcase()
+s= 'Learning python is fun!'
+print(s.swapcase())
+
+#demo of capitalise()
+s= 'Learning python is fun!'
+print (s.capitalize())
+
+
+#demo of title()
+s= 'Learning python is fun!'
+print(s.title())
+
+
+#demo of lstrip(()
+s= '        learning python is fun!'
+print(s.lstrip())
+s='*******learning python is fun!'
+print(s.lstrip('*'))
+
+#demo of rstrip()
+s= 'learning python is fun!'
+print(s.rstrip())
+s='learning python is fun!********'
+print(s.rstrip('*'))
+
+
+#demo of strip()
+s= '   learning python is fun!'
+print(s.strip())
+s='learning python is fun!********'
+print(s.strip('*'))
+
+
+
+#demo of max(str)()
+s= 'Learning python is fun!'
+print('maximum charater is :', max(s))
+
+#demo of min(str)()
+s= 'Learning python is fun!'
+print('minimum charater is :', min(s))
+
+
+
+
---
+++ stfu1.py
@@ -0,0 +1,14 @@
+s="This is very new.this is good"
+print(s.replace('is', 'was'))
+print(s.replace('is', 'was', 2))
+
+s="This is python programming"
+print(s.center(30, '*'))
+print(s.center(30))
+
+s="This is python programming"
+print(s.rjust(30, '*'))
+print(s.rjust(30))
+
+
+
