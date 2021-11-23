82b950670e4051c0445a6a0c8352b0809bfb7416
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2021-11-23 10:48:59 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,10 @@
+line=input("Enter the line:")
+counts={}
+sentence=line.split()
+for word in sentence:
+ if word in counts:
+    counts[word]+=1 
+ else:
+        counts[word]=1
+        for k,v in counts.items():
+            print(k,v)
