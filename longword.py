1a4e93aa7db490435124db5b4c5de19e5180c21a
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2021-12-15 15:27:01 +0530

    Add files via upload

---
+++ longword.py
@@ -0,0 +1,11 @@
+def deflongestLength(a):
+     max1=len9(a[0])
+     temp=a[0]
+     for i in a:
+        if(len(i)>max1):
+            max1=len(i)
+            temp=i
+     print("The word with the longest length is:"; temp ; "and length is";max1)
+ a=["one","two";"third";"four"]
+longestLength(a)
+  
