875f639546f7ae76f0f31796908eb05086ccc846
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2021-12-15 16:18:51 +0530

    Add files via upload

---
+++ int.py
@@ -0,0 +1,8 @@
+a=int(input("Enter an integer:"))
+n1=int("%s"%a)
+n2=int("%s%s"%(a,a))
+n3=int("%s%s%s"%(a,a,a))
+print(n1,"",n2,"",n3)
+print("sum=",(n1+n2+n3))
+
+                                                                                                                                                                                             0.                                 
