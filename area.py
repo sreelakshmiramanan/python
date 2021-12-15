9db06241895fdaeaaedc891c5d9b2d2fc900c8eb
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2021-12-15 16:18:34 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+import math
+s_area=lambada a:a**2
+r_area=lambda len, ht: len*ht
+t_area=lambda b,h : (1/2)*b*h
+print("Area of square (10)is:", s_area(10))
+print("Area of Rectangle(30,20)is:", r_area(30,20))
+print("Area of Tringle(10,20)is:", t_area(10,20))
