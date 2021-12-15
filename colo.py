87eca8871877c6a4b4c72048b7c85917ce892a35
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2021-12-15 13:56:29 +0530

    Add files via upload

---
+++ colo.py
@@ -0,0 +1,8 @@
+color=input("Enter colors(space between colors):")
+color_list=color.split(",")
+print(color_list)
+print("The first color of the list is:",color_list[0],"and the last color is:",color_list[-1])
+
+
+
+                                                                                                        
