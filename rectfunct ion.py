a7096ea0f2c10df4d32b55e69ce56f20bc315f0e
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2022-02-02 14:45:31 +0530

    Add files via upload

---
+++ rectfunct ion.py
@@ -0,0 +1,9 @@
+def RectArea(1,b):
+    return(1*b)
+def RectPerimeter(1,b):
+    return(2*(1+b))
+from math import pi
+def SpArea(r):
+    return(4*pi*r*r)
+def SpPerimeter(r):
+    return((4/3)*pi*r*r*r)
