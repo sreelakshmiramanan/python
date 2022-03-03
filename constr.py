cfa9a00904e3374a4fb46540a8b3b91566e0d83a
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2022-03-03 11:45:23 +0530

    Add files via upload

---
+++ constr.py
@@ -0,0 +1,10 @@
+number=3
+if number > 99 and number < 1000:
+  print('3 digit')
+else:
+  print('Not 3 digit')
+
+
+response = input('Are ypu familiar with python : ')
+if response.upper() == "yes":
+ 
