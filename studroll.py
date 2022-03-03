19a0e1981f7c75bfc62d6de27377c7642a00582d
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2022-03-03 11:54:00 +0530

    Add files via upload

---
+++ studroll.py
@@ -0,0 +1,19 @@
+class student:
+ studentcount = 0
+ def init(self,rino,name):
+  self.rino=rino
+  self.name=name
+  student.studentcount = 1
+def displaystudent(self):
+ print("rollno:",self.rino)
+ print("name",self.name)
+ stud1=student(10,"sahala")
+ stud2=student(11,"sree")
+ stud1.displaystudent()
+ student2.displaystudent()
+                                           
+
+
+
+
+                                           
