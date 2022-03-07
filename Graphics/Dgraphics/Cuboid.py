eb457b4bb3f5449472e39a251eff652b2c38d60c
sreelakshmiramanan <94426461+sreelakshmiramanan@users.noreply.github.com>
2022-03-07 06:46:48 +0530

    package

---
+++ Graphics/Dgraphics/Cuboid.py
@@ -0,0 +1,4 @@
+def cubesurface(l,b,h):
+    return (2*(l*b + b*h + l*h))
+def cubelateral(l,b,h):
+    return (2*h*(l + b) )
---
+++ Graphics/Dgraphics/Sphere.py
@@ -0,0 +1,6 @@
+from math import pi
+def SphereArea(r):
+    return (4*pi*r*r)
+
+def SpherePerimeter(r):
+    return ((4/3)*pi*r*r*r)
